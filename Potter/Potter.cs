dcb1dfda48cbb0a5669e4019b7c5709b2d618ec5
darkiri <darkiri@gmail.com>
2011-09-15 09:11:25 +0200

    Two four-four book piles are cheaper as five-three piles (thanx @bene331)
    Simple solution - move the fifth book to the pile with three books

--- Potter/Potter.cs
+++ Potter/Potter.cs
@@ -2,11 +2,9 @@
 using System.Linq;
 using NUnit.Framework;
 
-namespace Potter
-{
+namespace Potter {
     [TestFixture]
-    public class PotterTests
-    {
+    public class PotterTests {
         private void AssertCalculatePrice(Dictionary<Potter, int> soldBooks, double expectedPrice) {
             var calc = new PriceCalculator();
             Assert.That(calc.GetPrice(soldBooks), Is.EqualTo(expectedPrice));
@@ -71,8 +69,30 @@ namespace Potter
                 {Potter.Book3, 1},
                 {Potter.Book4, 1},
                 {Potter.Book5, 1},
-            }, 5 * 8 * .75 + 2 * 8 * .95 + 2 * 8);
-        }        
+            }, 5*8*.75 + 2*8*.95 + 2*8);
+        }
+
+        [Test]
+        public void FourFourCheaperAsFiveThree() {
+            AssertCalculatePrice(new Dictionary<Potter, int> {
+                {Potter.Book1, 2},
+                {Potter.Book2, 2},
+                {Potter.Book3, 2},
+                {Potter.Book4, 1},
+                {Potter.Book5, 1},
+            }, 4*8*.8 + 4*8*.8);
+        }
+
+        [Test]
+        public void FourFourFourFourCheaperAsFiveThreeFiveThree() {
+            AssertCalculatePrice(new Dictionary<Potter, int> {
+                {Potter.Book1, 4},
+                {Potter.Book2, 4},
+                {Potter.Book3, 4},
+                {Potter.Book4, 2},
+                {Potter.Book5, 2},
+            }, 4*8*.8 + 4*8*.8 + 4*8*.8 + 4*8*.8);
+        }
     }
 
     public enum Potter {
@@ -83,28 +103,43 @@ namespace Potter
         Book5
     }
 
-    public class PriceCalculator
-    {
+    public class PriceCalculator {
         private const double PRICE_PER_ITEM = 8;
 
         public double GetPrice(Dictionary<Potter, int> books) {
             var sortedPerDiscount = SortBooks(books, new List<Dictionary<Potter, int>>());
-            return sortedPerDiscount.Sum(s => PricePerPile(s));
+            return OptimizeDiscounts(sortedPerDiscount).Sum(s => PricePerPile(s));
+        }
+
+        private IEnumerable<Dictionary<Potter, int>> OptimizeDiscounts(IEnumerable<Dictionary<Potter, int>> books) {
+            return books.Any(pile => pile.Count == 5) &&
+                   books.Any(pile => pile.Count == 3)
+                       ? OptimizeDiscounts(Optimize5And3Piles(books))
+                       : books;
+        }
+
+        private IEnumerable<Dictionary<Potter, int>> Optimize5And3Piles(IEnumerable<Dictionary<Potter, int>> books) {
+            var fiveBooksPile = books.First(b => b.Count == 5);
+            var threeBooksPile = books.First(b => b.Count == 3);
+            var bookToMove = fiveBooksPile.First(b => !threeBooksPile.Keys.Contains(b.Key));
+            fiveBooksPile.Remove(bookToMove.Key);
+            threeBooksPile[bookToMove.Key] = bookToMove.Value;
+            return books;
         }
 
         private double PricePerPile(Dictionary<Potter, int> books) {
-            return books.Sum(b => b.Value * PRICE_PER_ITEM) * GetDiscount(books.Count());
+            return books.Sum(b => b.Value*PRICE_PER_ITEM)*GetDiscount(books.Count());
         }
 
-        public IEnumerable<Dictionary<Potter, int>> SortBooks(Dictionary<Potter, int> notYetSorted, 
+        public IEnumerable<Dictionary<Potter, int>> SortBooks(Dictionary<Potter, int> notYetSorted,
                                                               List<Dictionary<Potter, int>> sortedPerDiscount) {
             var newPile = notYetSorted.ToDictionary(b => b.Key, b => 1);
-            var remain = notYetSorted.Where(b => b.Value > 1).ToDictionary(b => b.Key, b => b.Value-1);
+            var remain = notYetSorted.Where(b => b.Value > 1).ToDictionary(b => b.Key, b => b.Value - 1);
 
             sortedPerDiscount.Add(newPile);
             return remain.Count > 0
-                ? SortBooks(remain, sortedPerDiscount)
-                : sortedPerDiscount;
+                       ? SortBooks(remain, sortedPerDiscount)
+                       : sortedPerDiscount;
         }
 
         public double GetDiscount(int bookCount) {
