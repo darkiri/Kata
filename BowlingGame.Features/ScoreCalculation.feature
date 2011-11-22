2b54fa76a4bcaed6e28d38799b2ef9565495b925
darkiri <darkiri@gmail.com>
2011-11-22 23:35:01 +0100

    Some fun with the SpecFlow

---
+++ BowlingGame.Features/ScoreCalculation.feature
@@ -0,0 +1,56 @@
+﻿Feature: Score Calculation
+	In order to know my performance
+	As a player
+	I want the system to calculate my total score
+
+Background:
+	Given a new bowling game 
+	 
+Scenario: Gutter Game
+	When all of my balls are landing in the gutter
+	Then my total score should be 0
+
+Scenario: Single Roll Scores
+	When one roll scores single pin
+	 And all other balls are landing in the gutter
+	Then my total score should be 1
+
+Scenario: Every Roll Scores
+	When all rolls score one pin
+	Then my total score should be 20
+
+Scenario: Single Spare Scores
+	When i hit 5 and 5 pins in a frame
+	 And i hit 1 and 2 pins in the next frame
+	 And all other balls are landing in the gutter
+	Then my total score should be 14
+
+Scenario: Double Spare Scores
+	When i hit 5 and 5 pins in the first frame
+	 And i hit 4 and 6 pins in the next frame
+	 And i hit 2 and 3 pins in the following frame
+	 And all other balls are landing in the gutter
+	Then my total score should be 31
+
+Scenario: Single Strike Scores
+	When i hit 10 in a frame
+	 And i hit 1 and 2 pins in the next frame
+	 And all other balls are landing in the gutter
+	Then my total score should be 16
+
+Scenario: Zero/Ten Scores as a Spare
+	When i hit 0 and 10 pins in a frame
+	 And i hit 1 and 2 pins in the next frame
+	 And all other balls are landing in the gutter
+	Then my total score should be 14
+
+Scenario: Double Strike Scores
+	When i hit 10 in a frame
+	 And i hit 10 in the next frame
+	 And i hit 1 and 2 pins in the following frame
+	 And all other balls are landing in the gutter
+	Then my total score should be 37
+
+Scenario: Perfect Game
+	When all of my rolls are strikes
+	Then my total score should be 300
---
+++ BowlingGame.Features/ScoreCalculationSteps.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace BowlingGame.Features
+{
+    [Binding]
+    public class ScoreCalculationSteps
+    {
+        private Game _game;
+        private int _rollsMade;
+
+        [Given(@"a new bowling game")]
+        public void GivenANewBowlingGame()
+        {
+            _game = new Game();
+        }
+
+        [When(@".+ balls are landing in the gutter")]
+        public void WhenAllOfMyBallsAreLandingInTheGutter()
+        {
+            RollMany(20-_rollsMade, 0);
+        }
+
+        private void RollMany(int times, int pins)
+        {
+            for (var i = 0; i < times; i++)
+            {
+                _game.Roll(pins);
+            }
+        }
+
+        [When(@"one roll scores single pin")]
+        public void WhenOneRollScoresSinglePin()
+        {
+            _game.Roll(1);
+            _rollsMade = 1;
+        }
+
+        [When(@"all rolls score one pin")]
+        public void WhenAllRollsScoreOnePin()
+        {
+            RollMany(20, 1);
+        }
+
+        [When(@"i hit (\d+) and (\d+) pins in .+ frame")]
+        public void WhenIRollAFrame(int pins1, int pins2)
+        {
+            _game.Roll(pins1);
+            _game.Roll(pins2);
+            _rollsMade = 2;
+        }
+
+        [When(@"i hit 10 in .+ frame")]
+        public void WhenIRollAStrike()
+        {
+            _game.Roll(10);
+            _rollsMade = 2;
+        }
+
+        [When(@"all of my rolls are strikes")]
+        public void WhenAllRollsAreStrikes()
+        {
+            RollMany(12, 10);
+        }
+
+        [Then(@"my total score should be (\d+)")]
+        public void ThenMyTotalScoreShouldBe(int score)
+        {
+            Assert.That(_game.Score(), Is.EqualTo(score));
+        }
+    }
+}
